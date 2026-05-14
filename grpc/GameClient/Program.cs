eedb5da59afec45256f8b0aaf3db751243143d58
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-14 12:09:15 +0300

    feat: implement service on client

--- grpc/GameClient/Program.cs
+++ grpc/GameClient/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace GameClient
+﻿using GameServer.Protos;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace GameClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Game.GameClient(channel);
+
+            using var call = client.JoinGame();
+
+            var sendTask = Task.Run(async () =>
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    await call.RequestStream.WriteAsync(new PlayerAction
+                    {
+                        PlayerName = "Ahmed",
+                        Answer = $"Answer {i}"
+                    });
+
+                    await Task.Delay(1000);
+                }
+
+                await call.RequestStream.CompleteAsync();
+            });
+
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var msg in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine($"SERVER: {msg.CurrentQuestion} | {msg.RemainingSeconds}");
+                }
+            });
+
+            await Task.WhenAll(sendTask, readTask);
         }
     }
 }
---
+++ grpc/GameServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using GameServer.Protos;
+using GameServer.Services;
+using Grpc.Core;
+
+namespace GameServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var server = new Server
+            {
+                Services = { Game.BindService(new GameService()) },
+                Ports = { new ServerPort("localhost", 5000, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+        }
+    }
+}
