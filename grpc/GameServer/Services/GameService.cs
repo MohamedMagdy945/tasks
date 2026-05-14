3130195a387a0535b8edd4cf9d804624159096c2
Mohamed Magdy <mohamedmagdy9452@gmail.com>
2026-05-14 11:55:37 +0300

    feat: implement service on server

---
+++ grpc/GameServer/Services/GameService.cs
@@ -0,0 +1,44 @@
+﻿using GameServer.Protos;
+using Grpc.Core;
+
+namespace GameServer.Services
+{
+    public class GameService : Game.GameBase
+    {
+        public override async Task JoinGame(
+          IAsyncStreamReader<PlayerAction> requestStream,
+          IServerStreamWriter<GameEvent> responseStream,
+          ServerCallContext context)
+        {
+            var cts = context.CancellationToken;
+
+            var readTask = Task.Run(async () =>
+            {
+                while (await requestStream.MoveNext())
+                {
+                    var action = requestStream.Current;
+
+                    Console.WriteLine($"{action.PlayerName}: {action.Answer}");
+                }
+            });
+
+            var remaining = 10;
+
+            while (!cts.IsCancellationRequested && remaining >= 0)
+            {
+                await responseStream.WriteAsync(new GameEvent
+                {
+                    CurrentQuestion = "What is 2 + 2 ?",
+                    RemainingSeconds = remaining,
+                    Leaderboard = "Ahmed:10, Ali:8",
+                    Winner = ""
+                });
+
+                remaining--;
+                await Task.Delay(1000);
+            }
+
+            await readTask;
+        }
+    }
+}
